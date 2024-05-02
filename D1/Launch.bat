631a1f1bd43577df2184c048fed27d0a81eff029
Sierra <leaprealm@gmail.com>
2024-05-02 17:16:19 +0900

    배치파일 업데이트

--- D1/Launch.bat
+++ D1/Launch.bat
@@ -1,18 +1,13 @@
 dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
--Mode=QueryTargets ^
--Output="%~dp0Intermediate\TargetInfo.Engine.json"
+-Mode=QueryTargets -Output="%~dp0Intermediate\TargetInfo.Engine.json"
 
 dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
--Mode=QueryTargets ^
--Output="%~dp0Intermediate\TargetInfo.D1.json" ^
--Project="%~dp0D1.uproject"
+-Mode=QueryTargets -Output="%~dp0Intermediate\TargetInfo.D1.json" -Project="%~dp0D1.uproject"
 
 dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
--ProjectFiles -VisualStudio -Automated -Minimize -Platforms=Win64 -TargetTypes=Editor ^
--TargetConfigurations=Development "%~dp0D1.uproject"
+-ProjectFiles -VisualStudio -Automated -Minimize -Platforms=Win64 -TargetTypes=Editor -TargetConfigurations=Development "%~dp0D1.uproject"
 
 call "C:\Program Files\Epic Games\UE_5.4\Engine\Build\BatchFiles\Build.bat" ^
-D1Editor Win64 Development -Project="%~dp0\D1.uproject" 
--WaitMutex -FromMsBuild -architecture=x64
+D1Editor Win64 Development -Project="%~dp0\D1.uproject" -WaitMutex -FromMsBuild -architecture=x64
 
 call "%~dp0\D1.uproject"
