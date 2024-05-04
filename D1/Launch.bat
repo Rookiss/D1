bbe36a979cc39a48ed86f40e9f3c4d9feb748231
Sierra <leaprealm@gmail.com>
2024-05-05 03:25:05 +0900

    배치파일 업데이트

--- D1/Launch.bat
+++ D1/Launch.bat
@@ -1,5 +1,8 @@
-dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
--ProjectFiles -Automated -Minimize -Platforms=Win64 -TargetTypes=Editor -TargetConfigurations=Development "%~dp0D1.uproject"
+@REM dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
+@REM -ProjectFiles -Automated -Minimize -Platforms=Win64 -TargetTypes=Editor -TargetConfigurations=Development "%~dp0D1.uproject"
+
+call "C:/Program Files/Epic Games/UE_5.4/Engine/Build/BatchFiles/Build.bat" ^
+-projectfiles -project="%~dp0\D1.uproject" -game -rocket -progress
 
 call "C:\Program Files\Epic Games\UE_5.4\Engine\Build\BatchFiles\Build.bat" ^
 D1Editor Win64 Development -Project="%~dp0\D1.uproject" -WaitMutex -FromMsBuild -architecture=x64
