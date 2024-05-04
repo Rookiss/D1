490d0f430e9e4a15c319ddbf4438a4307cd4bafd
Jinwoo Kim <leaprealm@gmail.com>
2024-05-05 03:16:38 +0900

    배치파일 업데이트

--- D1/Launch.bat
+++ D1/Launch.bat
@@ -1,11 +1,5 @@
 dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
--Mode=QueryTargets -Output="%~dp0Intermediate\TargetInfo.Engine.json"
-
-dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
--Mode=QueryTargets -Output="%~dp0Intermediate\TargetInfo.D1.json" -Project="%~dp0D1.uproject"
-
-dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
--ProjectFiles -VisualStudio -Automated -Minimize -Platforms=Win64 -TargetTypes=Editor -TargetConfigurations=Development "%~dp0D1.uproject"
+-ProjectFiles -Automated -Minimize -Platforms=Win64 -TargetTypes=Editor -TargetConfigurations=Development "%~dp0D1.uproject"
 
 call "C:\Program Files\Epic Games\UE_5.4\Engine\Build\BatchFiles\Build.bat" ^
 D1Editor Win64 Development -Project="%~dp0\D1.uproject" -WaitMutex -FromMsBuild -architecture=x64
