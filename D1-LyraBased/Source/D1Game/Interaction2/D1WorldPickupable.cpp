80305c458af88222d923d7cb9405604c5daf6040
silver <yieun0824@naver.com>
2024-07-01 19:17:11 +0900

    Merge

---
+++ D1-LyraBased/Source/D1Game/Interaction2/D1WorldPickupable.cpp
@@ -0,0 +1,14 @@
+﻿#include "D1WorldPickupable.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(D1WorldPickupable)
+
+AD1WorldPickupable::AD1WorldPickupable(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	
+}
+
+void AD1WorldPickupable::GatherInteractionInfos(const FD1InteractionQuery& InteractionQuery, FD1InteractionInfoBuilder& InteractionInfoBuilder) const
+{
+	InteractionInfoBuilder.AddInteractionInfo(InteractionInfo);
+}
---
+++ D1-LyraBased/Source/D1Game/Item/Managers/D1ItemManagerComponent.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "Components/ControllerComponent.h"
+#include "D1ItemManagerComponent.generated.h"
+
+class UD1EquipmentManagerComponent;
+class UD1InventoryManagerComponent;
+
+UCLASS()
+class UD1ItemManagerComponent : public UControllerComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UD1ItemManagerComponent(const FObjectInitializer& ObjectInitializer);
+	
+public:
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void Server_InventoryToEquipment(UD1InventoryManagerComponent* FromInventoryManager, const FIntPoint& FromItemSlotPos, UD1EquipmentManagerComponent* ToEquipmentManager, EEquipmentSlotType ToEquipmentSlotType);
+
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void Server_EquipmentToInventory(UD1EquipmentManagerComponent* FromEquipmentManager, EEquipmentSlotType FromEquipmentSlotType, UD1InventoryManagerComponent* ToInventoryManager, const FIntPoint& ToItemSlotPos);
+
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void Server_InventoryToInventory(UD1InventoryManagerComponent* FromInventoryManager, const FIntPoint& FromItemSlotPos, UD1InventoryManagerComponent* ToInventoryManager, const FIntPoint& ToItemSlotPos);
+	
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void Server_EquipmentToEquipment(UD1EquipmentManagerComponent* FromEquipmentManager, EEquipmentSlotType FromEquipmentSlotType, UD1EquipmentManagerComponent* ToEquipmentManager, EEquipmentSlotType ToEquipmentSlotType);
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly)
+	void AddAllowedComponent(UActorComponent* ActorComponent);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly)
+	void RemoveAllowedComponent(UActorComponent* ActorComponent);
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, BlueprintPure)
+	bool IsAllowedComponent(UActorComponent* ActorComponent) const;
+	
+private:
+	UPROPERTY()
+	TArray<TWeakObjectPtr<UActorComponent>> AllowedComponents;
+};
