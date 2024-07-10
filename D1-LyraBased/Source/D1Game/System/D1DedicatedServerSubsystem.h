390bb4e38d529dca661375c83cbadbd22cc21992
Sierra <leaprealm@gmail.com>
2024-07-10 14:02:05 +0900

    패키징 에러 수정

---
+++ D1-LyraBased/Source/D1Game/System/D1DedicatedServerSubsystem.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "D1DedicatedServerSubsystem.generated.h"
+
+UCLASS()
+class UD1DedicatedServerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UD1DedicatedServerSubsystem();
+};
