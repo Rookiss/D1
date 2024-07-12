16be421536ca876becfdede305ee3b66ac867868
Sierra <leaprealm@gmail.com>
2024-07-12 17:50:03 +0900

    수정

--- D1-LyraBased/Source/D1Game/AbilitySystem/Abilities/LyraGameplayAbility_Jump.cpp
+++ D1-LyraBased/Source/D1Game/AbilitySystem/Abilities/LyraGameplayAbility_Jump.cpp
@@ -18,13 +18,13 @@ bool ULyraGameplayAbility_Jump::CanActivateAbility(const FGameplayAbilitySpecHan
 	{
 		return false;
 	}
-
+	
 	const ALyraCharacter* LyraCharacter = Cast<ALyraCharacter>(ActorInfo->AvatarActor.Get());
-	if (!LyraCharacter || !LyraCharacter->CanJump())
-	{
-		return false;
-	}
-
+	// if (!LyraCharacter || !LyraCharacter->CanJump())
+	// {
+	// 	return false;
+	// }
+	//
 	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
 	{
 		return false;
