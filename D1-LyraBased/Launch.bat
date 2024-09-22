f20460a44d974559f6ee963ed09658a863901b5e
Jinwoo Kim <leaprealm@gmail.com>
2024-09-22 22:58:57 +0900

    배치파일 수정

--- D1-LyraBased/Launch.bat
+++ D1-LyraBased/Launch.bat
@@ -1,8 +1,3 @@
-@REM dotnet "C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" ^
-@REM -ProjectFiles -Automated -Minimize -Platforms=Win64 -TargetTypes=Editor -TargetConfigurations=Development "%~dp0D1.uproject"
-
-git update-index --assume-unchanged "%~dp0\D1.uproject"
-
 call "C:/Program Files/Epic Games/UE_5.4/Engine/Build/BatchFiles/Build.bat" ^
 -projectfiles -project="%~dp0\D1.uproject" -game -rocket -progress
 
